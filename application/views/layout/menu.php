0c88afe9ae1dafeb8211f4d38a26f2b48549cfc2
PvtWendy <wendyhemlock@gmail.com>
2023-10-18 21:40:46 -0400

    added layout

---
+++ application/views/layout/menu.php
@@ -0,0 +1,12 @@
+<html>
+
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">Coisa</a>
+        <a href="#">Coisa</a>
+    </nav>
+
+</html>
