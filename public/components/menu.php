f3e9571570c2db04b3852236ac5e8e63746599c0
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2023-11-10 20:57:03 -0400

    Spelling

--- public/components/menu.php
+++ public/components/menu.php
@@ -1,5 +1,5 @@
 <nav class="topNav">
     <a href="/home/index">Home</a>
-    <a href="/product/index">Produto</a>
-    <a href="#">Coisa</a>
+    <a href="/product/index">Products</a>
+    <a href="#">Placeholder</a>
 </nav>
