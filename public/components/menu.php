c16d0e5dcb4e3629bd49a3ce39636919279b9f77
PvtWendy <wendyhemlock@gmail.com>
2023-11-22 20:01:40 -0400

    login button

--- public/components/menu.php
+++ public/components/menu.php
@@ -1,5 +1,16 @@
 <nav class="topNav">
-    <a href="/home/index">Home</a>
-    <a href="/product/index">Products</a>
-    <a href="#">Placeholder</a>
+    <div>
+        <a href="/home/index">Home</a>
+        <a href="/product/index">Products</a>
+        <a href="#">Placeholder</a>
+    </div>
+    <div>
+        <button>
+            Login
+        </button>
+        <button>
+            Signup
+        </button>
+    </div>
+
 </nav>
