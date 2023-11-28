950889914d683dbf744fd000d8ea5ce8fa26f1be
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2023-11-28 19:31:45 -0400

    Removed useless shit

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -12,15 +12,11 @@
     <?php
     $componentDir = $_SERVER["DOCUMENT_ROOT"] . "/components/";
     include $componentDir . "menu.php"; ?>
-    <form action="/product/submit" method="POST" class="productForm">
+    <form action="/user/sendLogin" method="POST" class="productForm">
         <label for="user_name">Name:</label>
         <input type="text" name="user_name">
         <label for="user_password">Password:</label>
-        <input type="text" name="user_password  ">
-        <label for="user_number">Phone Number:</label>
-        <input type="text" name="user_number">
-        <label for="user_address">Address:</label>
-        <input type="text" name="user_address">
+        <input type="password" name="user_password">
         <input type="submit" value="Submit" id="submitButton">
     </form>
 </body>
