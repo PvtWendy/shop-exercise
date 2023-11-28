83e0f052cf162d55b88098e136dd62cd8cdb1000
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2023-11-28 19:32:22 -0400

    Added more stuff to controller

--- application/controllers/userController.php
+++ application/controllers/userController.php
@@ -1,8 +1,10 @@
 <?php
-use application\models\user;
-use application\core\controller;
+
+use application\models\User;
+use application\core\Controller;
 use application\DAO\UserDAO;
-class userController extends controller
+
+class UserController extends Controller
 {
     public function index()
     {
@@ -14,31 +16,90 @@ class userController extends controller
         $this->view("user/signup");
     }
 
-    public function sendSignup(){
+    public function sendSignup()
+    {
         $name = $_POST["user_name"];
         $password = $_POST["user_password"];
         $confirmation = $_POST["user_confirmation"];
         $number = $_POST["user_number"];
         $address = $_POST["user_address"];
 
-
-
         if ($password === $confirmation) {
-            $user = new user($name, $number,$address,$password);
+            $user = new User($name, $number, $address, $password);
 
             $userDAO = new UserDAO();
             $userDAO->signup($user);
-    
-            $this->view("user/index");    
-        }else{
-            echo("AAAAAAAAAAAA");
+
+            header("Location: /user/login");
+            exit();
+        } else {
+            echo "Passwords do not match!";
         }
-        
     }
-    
+
     public function login()
     {
         $this->view("user/login");
+    }
+
+    public function sendLogin()
+    {
+        $name = $_POST["user_name"];
+        $password = $_POST["user_password"];
+
+        $userDAO = new UserDAO();
+        $loggedIn = $userDAO->login($name, $password);
+
+        if ($loggedIn) {
+            header("Location: /home/index");
+            exit();
+        } else {
+            echo "Login failed!";
+        }
+    }
+
+    public function update()
+    {
+        $userDAO = new UserDAO();
+        $user = $userDAO->getById($_SESSION['user_id']);
+        $this->view("user/update", ["user" => $user]);
+    }
+
+    public function submitUpdate()
+    {
+        $code = $_POST["user_code"];
+        $name = $_POST["user_name"];
+        $number = $_POST["user_number"];
+        $address = $_POST["user_address"];
+        $oldPassword = $_POST["old_password"]; // Assuming you have a field for old password in your form
+        $newPassword = $_POST["user_password"];
+    
+        $userDAO = new UserDAO();
+        $user = $userDAO->getById($code);
+    
+        if ($user && password_verify($oldPassword, $user->getPassword())) {
+            $user->setName($name);
+            $user->setNumber($number);
+            $user->setAddress($address);
+            $user->setPassword($newPassword);
+    
+            if ($userDAO->edit($user)) {
+                echo "User updated successfully!";
+                $this->view("user/index");
+            } else {
+                echo "Error updating user.";
+            }
+        } else {
+            echo "Old password does not match.";
+        }
+    }
+    public function delete()
+    {
+        $userDAO = new UserDAO();
+        $userDAO->delete();
+
+        header("Location: /home/index");
+        exit();
     }
 }
 
