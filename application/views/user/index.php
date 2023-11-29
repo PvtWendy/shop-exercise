df92b81570b4d79bd21215ed62929073d8def37f
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2023-11-28 20:29:01 -0400

    Made the User functions

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -4,13 +4,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>User Profile</title>
 </head>
 
 <body>
     <?php
     $componentDir = $_SERVER["DOCUMENT_ROOT"] . "/components/";
-    include $componentDir . "menu.php"; ?>
+    include $componentDir . "menu.php";
+    ?>
+
+    <form action="/user/signout" method="post">
+        <button type="submit" class="button">Sign Out</button>
+    </form>
+
+    <a href="/user/update">
+        <button class="button">Update Profile</button>
+    </a>
+
+    <form action="/user/delete" method="post">
+        <button type="button" class="button" onclick="openConfirmationDialog()">Delete Account</button>
+    </form>
+
+    <dialog id="confirmationDialog">
+        <p id="confirmationMessage">Are you sure you want to delete your account?</p>
+        <button onclick="confirmDelete()">Yes, delete</button>
+        <button onclick="closeConfirmationDialog()">Cancel</button>
+    </dialog>
+
+    <script>
+        const confirmationDialog = document.getElementById('confirmationDialog');
+
+        function openConfirmationDialog() {
+            confirmationDialog.showModal();
+        }
+
+        function closeConfirmationDialog() {
+            confirmationDialog.close();
+        }
+
+        function confirmDelete() {
+            document.querySelector('form[action="/user/delete"]').submit();
+            closeConfirmationDialog();
+        }
+    </script>
 </body>
 
 </html>
