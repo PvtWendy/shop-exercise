8e8aa2cda89cc306ff84a32939b06a81bf07b987
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2023-11-28 20:36:37 -0400

    added file_location

--- application/models/product.php
+++ application/models/product.php
@@ -1,49 +1,69 @@
-<?php 
+<?php
+
 namespace application\models;
-class Product {
+
+class Product
+{
     private $code;
     private $name;
     private $brand;
     private $price;
+    private $file_location;
 
-
-    public function __construct($name, $brand, $price){
+    public function __construct($name, $brand, $price)
+    {
         $this->name = $name;
         $this->brand = $brand;
         $this->price = $price;
     }
 
-    public function setCode($code) {
+    public function setCode($code)
+    {
         $this->code = $code;
     }
 
-    public function getCode() {
+    public function getCode()
+    {
         return $this->code;
     }
 
-    public function getName() {
-        return  $this->name ;
+    public function getName()
+    {
+        return $this->name;
     }
 
-    public function setName($name){
+    public function setName($name)
+    {
         $this->name = $name;
     }
 
-    public function getBrand() {
+    public function getBrand()
+    {
         return $this->brand;
     }
 
-    public function setBrand($brand){
+    public function setBrand($brand)
+    {
         $this->brand = $brand;
     }
 
-    public function getPrice() {
+    public function getPrice()
+    {
         return $this->price;
     }
 
-    public function setPrice($price){
+    public function setPrice($price)
+    {
         $this->price = $price;
     }
-}
 
-?>
+    public function getFileLocation()
+    {
+        return $this->file_location;
+    }
+
+    public function setFileLocation($file_location)
+    {
+        $this->file_location = $file_location;
+    }
+}
